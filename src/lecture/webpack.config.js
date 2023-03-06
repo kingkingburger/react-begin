9d8d12b8fc8b82f9fb2a25fccf70a9bcdec693bd
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-03-06 22:05:27 +0900

    feat: How to set up webpack

--- src/lecture/webpack.config.js
+++ src/lecture/webpack.config.js
@@ -1,3 +1,18 @@
-module.exports ={
+const path = require('path')
 
+module.exports ={
+    name: 'wordreplay-setting',
+    mode: 'development', // 실서비스: production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', 'jsx']
+    },
+    // 중요!
+    entry:{
+        app: ['./client.jsx']
+    }, // 입력
+    output: {
+        path: path.join(__dirname, 'dist'), // 현재폴더경로(C:\user\webstorm\react-practice\...)/dist
+        filename: 'app.js'
+    } // 출력
 }
