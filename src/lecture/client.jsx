cd5a57a87cc396bc6709d3422e08953ee6b777ea
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-03-10 20:48:26 +0900

    learn: create workRelay in class

--- src/lecture/client.jsx
+++ src/lecture/client.jsx
@@ -1,5 +1,5 @@
 const React = require('react')
-const ReactDom = require('react-dom')
+const ReactDom = require('react-dom/client')
 
 const WordRelay = require('./WordRelay')
 
--- src/lecture/webpack.config.js
+++ src/lecture/webpack.config.js
@@ -1,29 +1,42 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports ={
-    name: 'word-relay-dev',
-    mode: 'development', // 실서비스: production
-    devtool: 'eval',
-    resolve: {
-        extensions: ['.js', '.jsx']
+
+    mode:'development',
+    devtool:'eval', // 개발때는 eval, 배포에서는 hidden-source-map
+    resolve:{
+        extensions: ['.js','.jsx'] // entry.app에서 확장자 생략 가능
     },
-    // 중요!
+
     entry:{
         app: ['./client']
-    }, // 입력
-
-    module: {
-        rules:[{ //여러개의 규칙들
-            test: /\.jsx?/, //js,jsx 파일에 rulse을 적용하겠다!
-            loader: 'babel-loader', // babel과 webpack 연결
-            options:{
-                presets:['@babel/preset-env', '@babel/preset-react']
-            }
-        }]
     },
 
-    output: {
-        path: path.join(__dirname, 'dist'), // 현재폴더경로(C:\user\webstorm\react-practice\...)/dist
-        filename: 'app.js'
-    } // 출력
+    module:{
+        rules:[{
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            options:{
+                presets:[
+                    ['@babel/preset-env', { //자동으로 옛날 브라우저 지원하는애
+                        targets:{
+                            browsers: ['> 5% in KR'],
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react'
+                ]
+            }
+        }],
+    },
+
+    plugins:[
+        new webpack.LoaderOptionsPlugin({debug: true}),
+    ],
+
+    output:{
+        filename:'app.js',
+        path: path.join(__dirname,'dist'),
+    }
 }
