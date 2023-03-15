393845bf43df942a5ea3efa1125ea5dd8f7f42aa
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-03-15 20:44:43 +0900

    fix: use map in react

--- src/lecture/NumberBaseball.jsx
+++ src/lecture/NumberBaseball.jsx
@@ -1,10 +1,48 @@
 // const React = require('React')
 // const {Component} = React
 import React, {Component} from "react";
-class NumberBaseball extends Component{
+
+function getNumbers() { // 숫자 4개를 랜덤하게 뽑아주는 함수
 
 }
 
+class NumberBaseball extends Component {
 
-export const hello = 'hello' // import { hello }
+    state = {
+        result: '',
+        value: '',
+        answer: getNumbers(),
+        tries: [],
+    }
+
+    onSubmitForm = () =>{
+
+    }
+
+    onChangeInput = () =>{
+
+    }
+
+    render() {
+        return (
+            <>
+                <h1>{this.state.result}</h1>
+                <form onSubmit={this.onSubmitForm}>
+                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+                </form>
+                <div>시도: {this.state.tries.length}</div>
+                <ul>
+                    {['like1','like2','like3'].map((v) =>{
+                        return(
+                            <li>{v}</li>
+                        )
+                    })}
+                </ul>
+            </>
+        )
+    }
+}
+
+
+// export const hello = 'hello' // import { hello }
 export default NumberBaseball // import Number Baseball
--- src/lecture/client.jsx
+++ src/lecture/client.jsx
@@ -1,7 +1,5 @@
-// const React = require('react')
 import React from 'react'
-// const ReactDom = require('react-dom/client')
 import ReactDom  from 'react-dom/client'
-const NumberBaseball = require('./NumberBaseball')
 
+import NumberBaseball from "./NumberBaseball";
 ReactDom.createRoot(document.querySelector('#root')).render(<NumberBaseball/>);
