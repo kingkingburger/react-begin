377c01e16dbd2c953411e541ffea17b7a4131ffe
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-03-20 20:32:35 +0900

    learn: child component

---
+++ src/lecture/Try.jsx
@@ -0,0 +1,12 @@
+import { memo } from "react";
+
+const Try = memo(({ tryInfo }) => {
+  return (
+    <li>
+      <div>{tryInfo.try}</div>
+      <div>{tryInfo.result}</div>
+    </li>
+  );
+});
+Try.displayName = "Try";
+export default Try;
