3ea73c94cdd705bbe4501b5fcd7aa1d541d4188f
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-03-23 20:06:40 +0900

    learn: How to Optimize Rendering!

--- src/lecture/client.jsx
+++ src/lecture/client.jsx
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom/client";
 
 import NumberBaseballClass from "./NumberBaseballClass";
 import NumberBaseball from "./NumberBaseball";
+import Test from "./RenderTest";
 
-ReactDOM.createRoot(document.querySelector("#root")).render(<NumberBaseball />);
+ReactDOM.createRoot(document.querySelector("#root")).render(<Test />);
