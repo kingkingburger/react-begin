6f72ae0be0b3e77d4fdaec461423981ff7160c68
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-03-30 22:49:10 +0900

    learn: Fixing ref error

--- src/lecture/client.jsx
+++ src/lecture/client.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import NumberBaseballClass from "./NumberBaseballClass";
-import NumberBaseball from "./NumberBaseball";
-import Test from "./RenderTest";
 import ResponseCheck from "./ResponseCheck";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(<ResponseCheck />);
