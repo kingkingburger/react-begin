9810d889d614d6fffa0173c8cc7ff99042940423
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-04-03 21:33:26 +0900

    learn: use setInterval in componentDidMount for change image

--- src/5.가위바위보/RSPClass.jsx
+++ src/5.가위바위보/RSPClass.jsx
@@ -4,20 +4,53 @@ import React, { Component } from "react";
 // setState/props 바뀔 때, shouldComponentUpdate => render => componentDidUpdate
 // 부모가 나를 없앴을 때 => componentWillUnmount => 소멸
 
+const rspCoords = {
+  바위: "0",
+  가위: "-142px",
+  보: "-284px",
+};
+
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
+
 class RSPClass extends Component {
   state = {
     result: "",
-    imgCoord: 0,
+    imgCoord: "0",
     score: 0,
   };
-  componentDidMount() {// 컴포넌트가 첫 렌더링된 후
 
+  interval;
+  componentDidMount() {
+    // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이 해요
+    this.interval = setInterval(() => {
+      const { imgCoord } = this.state;
+      if (imgCoord === rspCoords.바위) {
+        this.setState({
+          imgCoord: rspCoords.가위,
+        });
+      } else if (imgCoord === rspCoords.가위) {
+        this.setState({
+          imgCoord: rspCoords.보,
+        });
+      } else if (imgCoord === rspCoords.보) {
+        this.setState({
+          imgCoord: rspCoords.바위,
+        });
+      }
+    }, 1000);
   }
 
-  componentDidUpdate() { // 리랜더링 후
+  componentDidUpdate() {
+    // 리랜더링 후
   }
 
-  componentWillUnmount() { // 컴포넌트가 제거되기 직전
+  componentWillUnmount() {
+    // 컴포넌트가 제거되기 직전, 여기에 비동기 요청 정리를 많이 해요
+    clearInterval(this.interval);
   }
 
   onClickBtn() {}
