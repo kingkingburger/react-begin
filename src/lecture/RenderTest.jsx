1d2fe30c6d5fbc5e6797536b2116d4b61d3af236
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-04-10 21:23:56 +0900

    learn: why use memo: make pureComponent

--- src/lecture/RenderTest.jsx
+++ src/lecture/RenderTest.jsx
@@ -1,29 +1,22 @@
-import React, { Component } from "react";
+import React, { Component, useCallback, useEffect, useState } from "react";
 
-class Test extends Component {
-  state = {
-    counter: 0,
-  };
-  shouldComponentUpdate(nextProps, nextState, nextContext) {
-    if (this.state.counter !== nextState.counter) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+const Test = () => {
+  const [count, setCount] = useState(0);
 
-  onClick = () => {
-    this.setState({});
+  const handleClick = () => {
+    console.log("handleClick start:", count); // 1. 현재 count 값 출력
+    setCount(count + 1); // 2. count 상태값 변경
+    console.log("handleClick end:", count); // 3. 변경 전 count 값 출력
   };
 
-  render() {
-    console.log("렌더링", this.state);
-    return (
-      <div>
-        <button onClick={this.onClick}>클릭</button>
-      </div>
-    );
-  }
-}
+  console.log("render:", count); // 4. 렌더링 시점에 현재 count 값 출력
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click me</button>
+      <p>Count: {count}</p>
+    </div>
+  );
+};
 
 export default Test;
--- src/lecture/client.jsx
+++ src/lecture/client.jsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import ResponseCheck from "./ResponseCheck";
+import Test from "./RenderTest";
 
-ReactDOM.createRoot(document.querySelector("#root")).render(<ResponseCheck />);
+ReactDOM.createRoot(document.querySelector("#root")).render(<Test />);
