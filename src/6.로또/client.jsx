4f460cd9f42a03638593a203392f08ca4ba58bd7
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-04-13 20:30:58 +0900

    learn: LottoClass convert to hooks

--- src/6.로또/client.jsx
+++ src/6.로또/client.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import LottoClass from "./LottoClass";
+import Lotto from "./Lotto";
 
-ReactDOM.createRoot(document.querySelector("#root")).render(<LottoClass />);
+ReactDOM.createRoot(document.querySelector("#root")).render(<Lotto />);
