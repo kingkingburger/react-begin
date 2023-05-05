5d22cd32e135d5483f4976491b988afac8fdf1a5
DESKTOP-P678S41\wmh <dnjsalsgh123@gmail.com>
2023-05-06 08:36:09 +0900

    learn: make original gameMatcherClass

--- src/react-router/GameMacherClass.jsx
+++ src/react-router/GameMacherClass.jsx
@@ -7,18 +7,26 @@ import { Route, Routes, useLocation } from "react-router";
 
 class GameMacherClass extends Component {
   render() {
-    let urlSearchParams = new URLSearchParams(
-      this.props.location.search.slice(1)
-    );
-    console.log(urlSearchParams.get("page"));
-    return (
-      <Routes>
-        <Route path="number-baseball" element={<NumberBaseball />} />
-        <Route path="rock-scissors-paper" element={<RSP />} />
-        <Route path="lotto-generator" element={<Lotto />} />
-        <Route path="*" element={<div>일치하는 게임이 없습니다.</div>} />
-      </Routes>
-    );
+    // let urlSearchParams = new URLSearchParams(
+    //   this.props.location.search.slice(1)
+    // );
+    // console.log(urlSearchParams.get("page"));
+    // return (
+    //   <Routes>
+    //     <Route path="number-baseball" element={<NumberBaseball />} />
+    //     <Route path="rock-scissors-paper" element={<RSP />} />
+    //     <Route path="lotto-generator" element={<Lotto />} />
+    //     <Route path="*" element={<div>일치하는 게임이 없습니다.</div>} />
+    //   </Routes>
+    // );
+    if (this.props.match.params.name === "number-baseball") {
+      return <NumberBaseball />;
+    } else if (this.props.match.params.name === "rock-scissors-paper") {
+      return <RSP />;
+    } else if (this.props.match.params.name === "lotto-generator") {
+      return <Lotto />;
+    }
+    return <div>일치하는 게임이 없습니다.</div>;
   }
 }
 
